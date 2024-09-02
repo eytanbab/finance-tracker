991046faf2cd96bfcf8734ffa48f011ffefc39ca
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-03 00:26:25 +0300

    feat: replace placeholder text with button to create a new account

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <div>Dashboard page</div>;
+  const { isOpen, onOpen, onClose } = useNewAccount();
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
 }
