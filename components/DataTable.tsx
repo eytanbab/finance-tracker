17c49dba30f672f4c38b8fd117edc4f35a0e0c18
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-03 21:56:38 +0300

    feat: integrate useConfirm hook in DataTable component for handling delete confirmations

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -22,6 +22,8 @@ import {
   TableRow,
 } from '@/components/ui/table';
 
+import { useConfirm } from '@/hooks/use-confirm';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
@@ -43,6 +45,10 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmationDialog, confirm] = useConfirm(
+    'Are you sure?',
+    'You are about to perform a bulk delete'
+  );
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [rowSelection, setRowSelection] = useState({});
@@ -66,6 +72,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmationDialog />
       <div className='flex items-center py-4'>
         <Input
           placeholder={`Filter ${filterKey}...`}
@@ -81,6 +88,13 @@ export function DataTable<TData, TValue>({
             size='sm'
             variant='outline'
             className='ml-auto font-normal text-xs'
+            onClick={async () => {
+              const ok = await confirm();
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection();
+              }
+            }}
           >
             <Trash className='size-4 mr-2' />
             Delete ({table.getFilteredSelectedRowModel().rows.length})
