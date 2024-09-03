96565d061000a6ee56b1b4843a242e5c645e05ee
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-03 20:03:35 +0300

    feat: add placeholder AccountsPage

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+import { PlusIcon } from 'lucide-react';
+
+import { Payment, columns } from './columns';
+import { DataTable } from '../../../components/DataTable';
+
+const data: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com',
+  },
+  {
+    id: '453wrer11',
+    amount: 50,
+    status: 'processing',
+    email: 'a@example.com',
+  },
+];
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+  ];
+}
+
+const AccountsPage = () => {
+  const newAccount = useNewAccount();
+  return (
+    <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+      <Card className='border-none drop-shadow-sm'>
+        <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
+          <CardTitle className='text-xl line-clamp-1'>AccountsPage</CardTitle>
+          <Button onClick={newAccount.onOpen} size='sm'>
+            <PlusIcon className='size-4 mr-2' />
+            Add new
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={columns}
+            data={data}
+            filterKey='email'
+            onDelete={() => {}}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AccountsPage;
