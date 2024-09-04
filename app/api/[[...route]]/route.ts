e1dc10d0ada31d592e8d6e1ab007ebe4daedceef
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-04 20:43:12 +0300

    feat: add PATCH method handler

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -18,5 +18,7 @@ const routes = app.route('/accounts', accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+
 
 export type AppType = typeof routes;
