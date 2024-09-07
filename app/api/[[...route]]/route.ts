82677f2c9a57ff6dafbbec2f4f5de3f69cafcf47
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-07 19:37:52 +0300

    feat: add transactions route

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,6 +3,7 @@ import { handle } from 'hono/vercel';
 import { HTTPException } from 'hono/http-exception';
 import accounts from './accounts';
 import categories from './categories';
+import transactions from './transactions';
 
 export const runtime = 'edge';
 
@@ -18,7 +19,8 @@ app.onError((err, c) => {
 
 const routes = app
   .route('/accounts', accounts)
-  .route('/categories', categories);
+  .route('/categories', categories)
+  .route('/transactions', transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
