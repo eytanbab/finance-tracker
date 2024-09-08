097ddd07fe68d1dc6cfb6e8af131e74fdab51931
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-08 17:43:03 +0300

    fix: update imports to correct modules

--- app/(dashboard)/transactions/actions.tsx
+++ app/(dashboard)/transactions/actions.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useOpenAccount } from '@/features/accounts/hooks/use-open-account';
-import { useDeleteAccount } from '@/features/accounts/api/use-delete-account';
+import { useOpenTransaction } from '@/features/transactions/hooks/use-open-transaction';
+import { useDeleteTransaction } from '@/features/transactions/api/use-delete-transaction';
 
 import { useConfirm } from '@/hooks/use-confirm';
 
@@ -22,11 +22,11 @@ type Props = {
 export const Actions = ({ id }: Props) => {
   const [ConfirmDialog, confirm] = useConfirm(
     'Are you sure?',
-    'You are about to delete this account.'
+    'You are about to delete this transaction.'
   );
 
-  const { onOpen } = useOpenAccount();
-  const deleteMutation = useDeleteAccount(id);
+  const { onOpen } = useOpenTransaction();
+  const deleteMutation = useDeleteTransaction(id);
 
   const handleDelete = async () => {
     const ok = await confirm();
