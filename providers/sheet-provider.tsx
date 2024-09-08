00a5da64132b9867020cbc55fe63f21c86243744
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-08 17:51:37 +0300

    feat: add edit transaction sheet to sheet provider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -8,6 +8,7 @@ import NewCategorySheet from '@/features/categories/components/NewCategorySheet'
 import EditCategorySheet from '@/features/categories/components/EditCategorySheet';
 
 import NewTransactionSheet from '@/features/transactions/components/NewTransactionSheet';
+import EditTransactionSheet from '@/features/transactions/components/EditTransactionSheet';
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -23,6 +24,7 @@ export const SheetProvider = () => {
       <EditCategorySheet />
 
       <NewTransactionSheet />
+      <EditTransactionSheet />
     </>
   );
 };
