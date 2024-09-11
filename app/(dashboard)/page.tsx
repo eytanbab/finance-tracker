b87435e5a9c370b59c21d6daad4f9e509584347d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-11 23:43:39 +0300

    feat: add create new transaction button to overview page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,31 @@
+'use client';
+
 import { DataCharts } from '@/components/data-charts';
 import { DataGrid } from '@/components/data-grid';
+import { Button } from '@/components/ui/button';
+import { useGetSummary } from '@/features/summary/api/use-get-summary';
+import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction';
+import { PlusIcon } from 'lucide-react';
 
 export default function DashboardPage() {
+  const newTransaction = useNewTransaction();
+  const { isLoading } = useGetSummary();
+
   return (
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
       <DataGrid />
-      <DataCharts/>
+      {!isLoading && (
+        <Button
+          onClick={newTransaction.onOpen}
+          size='sm'
+          variant='outline'
+          className='w-full mx-auto lg:w-auto'
+        >
+          <PlusIcon className='size-4 mr-2' />
+          Add new tranasction
+        </Button>
+      )}
+      <DataCharts />
     </div>
   );
 }
