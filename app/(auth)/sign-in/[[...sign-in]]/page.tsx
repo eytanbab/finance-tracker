1795f12b33da75d9e383fd859581f19a9c09ef88
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-13 20:36:35 +0300

    fix: update styles for dark mode support

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -7,7 +7,9 @@ export default function Page() {
     <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
       <div className='h-full lg:flex flex-col items-center-justify-center px-4'>
         <div className='text-center space-y-4 pt-16'>
-          <h1 className='font-bold text-3xl text-[#2E2A47]'>Welcome back!</h1>
+          <h1 className='font-bold text-3xl text-[#2E2A47] dark:text-zinc-100'>
+            Welcome back!
+          </h1>
           <p className='text-base text-[#7E8CA0]'>
             Login in or Create account to get back to your dashboard!
           </p>
