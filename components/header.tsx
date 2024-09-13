f8f9758bc3bee965ad9984494b9da7f50898e11d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2024-09-13 20:22:06 +0300

    feat: add ThemeToggle component

--- components/header.tsx
+++ components/header.tsx
@@ -4,22 +4,26 @@ import Navigation from './navigation';
 import { Loader2 } from 'lucide-react';
 import WelcomeMsg from './welcome-msg';
 import { Filters } from './filters';
+import { ThemeToggle } from './theme-toggle';
 
 const Header = () => {
   return (
-    <header className='bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36'>
+    <header className='bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36 dark:from-gray-900 dark:to-gray-700'>
       <div className='max-w-screen-2xl mx-auto'>
         <div className='w-full flex items-center justify-between mb-14'>
           <div className='flex items-center lg:gap-x-16'>
             <HeaderLogo />
             <Navigation />
           </div>
-          <ClerkLoaded>
-            <UserButton />
-          </ClerkLoaded>
-          <ClerkLoading>
-            <Loader2 className='size-8 animate-spin text-slate-400' />
-          </ClerkLoading>
+          <div className='flex items-center gap-x-4'>
+            <ThemeToggle />
+            <ClerkLoaded>
+              <UserButton />
+            </ClerkLoaded>
+            <ClerkLoading>
+              <Loader2 className='size-8 animate-spin text-slate-400' />
+            </ClerkLoading>
+          </div>
         </div>
         <WelcomeMsg />
         <Filters />
